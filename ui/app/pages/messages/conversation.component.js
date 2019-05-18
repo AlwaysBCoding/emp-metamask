c436db298515484eb9958248437618b80e201ceb
Gilad <freeslugs@gmail.com>
2019-05-18 19:36:24 -0400

    wip activeconvo

--- ui/app/pages/messages/conversation.component.js
+++ ui/app/pages/messages/conversation.component.js
@@ -24,12 +24,7 @@ const myAddress = '0x7A33615d12A12f58b25c653dc5E44188D44f6898'
 
 const Tick = () => <span className="tick"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg></span>
 
-const Message = (props) => <div className={c('message', { received: (props.message.to === myAddress), sent: (props.message.from === myAddress) } )}>
-  { props.message.body }
-  <span className="metadata">
-    <span className="time">{ moment(props.message.createdAt).format('h:mm A') }<Tick /></span>
-  </span>
-</div>
+const Message = (props) => 
 
 class Conversation extends PureComponent {
   static propTypes = {
@@ -42,14 +37,43 @@ class Conversation extends PureComponent {
     t: PropTypes.func,
   }
 
+  state = {
+    message: "",
+    myAddress: '0x1563915e194D8CfBA1943570603F7606A3115508'
+  }
+
   componentWillMount = () => {
     const activeContactAddress = this.props.match.params.address
     const shortAddress = activeContactAddress.substring(0, 20)
     this.props.setPageTitle(shortAddress)
   }
 
-  encryptMessage = (message) => {
+  // encryptMessage = (message) => {
     
+  // }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(this.state.message)
+    this.setState({message: ""})
+  }
+
+  _renderMessage(message, index) {
+    return (
+      <div key={`message-${index}`} className={c('message', { received: (message.to === this.state.myAddress), sent: (message.from === this.state.myAddress) } )}>
+        { message.body }
+        <span className="metadata">
+          <span className="time">{ moment(message.createdAt).format('h:mm A') }<Tick /></span>
+        </span>
+      </div>
+    )
+  }
+
+  _renderMessages(messages) {
+    return messages.map((message, index) => {
+      return this._renderMessage(message, index);
+    })
   }
 
   render() {
@@ -67,15 +91,19 @@ class Conversation extends PureComponent {
         <div className="chat-container">
           <div className="conversation">
             <div className="conversation-container">
-              { messages.map((message, index) => <Message message={message} key={message.id} />) }  
+              {this._renderMessages(messages)}
             </div>
           </div>
           <div className='send-message'>
-            <TextField
-              className='send-message-textbox'
-              fullWidth
-              placeholder="Say something interesting"
-            />
+            <form onSubmit={this.onSubmit}>
+              <TextField
+                className='send-message-textbox'
+                fullWidth
+                placeholder="Say something interesting"
+                value={this.state.message}
+                onChange={(event) => this.setState({ message: event.target.value })}
+              />
+            </form>
           </div>
         </div>
       </div>
